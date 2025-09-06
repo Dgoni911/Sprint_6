dd384b7b4c97cfdee89057bbaab6de6127589440
Dgoni911 <dgoni911@gmail.com>
2025-09-06 20:38:07 +0100

    Merge remote changes

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+@pytest.fixture(scope='function')
+def driver():
+    options = Options()
+    options.add_argument('--headless')  
+    options.add_argument('--width=1920')
+    options.add_argument('--height=1080')
+    
+    driver = webdriver.Firefox(options=options)
+    driver.maximize_window()
+    
+    yield driver
+    driver.quit()
