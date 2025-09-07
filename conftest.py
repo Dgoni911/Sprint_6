ab5022498a882cd4a7fa8e611ec5748ca159957c
Dgoni911 <dgoni911@gmail.com>
2025-09-07 18:08:11 +0300

    1

--- conftest.py
+++ conftest.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
@@ -14,21 +13,4 @@ def driver():
     driver.maximize_window()
     
     yield driver
-=======
-import pytest
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
-@pytest.fixture(scope='function')
-def driver():
-    options = Options()
-    options.add_argument('--headless')  
-    options.add_argument('--width=1920')
-    options.add_argument('--height=1080')
-    
-    driver = webdriver.Firefox(options=options)
-    driver.maximize_window()
-    
-    yield driver
->>>>>>> 5e94532fc497709f8b899320f9d7b50e5d8148b5
     driver.quit()
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
@@ -47,54 +46,4 @@ class MainPage(BasePage):
         self.find_element(self.SCOOTER_LOGO).click()
     
     def click_yandex_logo(self):
-=======
-from selenium.webdriver.common.by import By
-from .base_page import BasePage
-
-class MainPage(BasePage):
-    # Локаторы для вопросов о важном
-    FAQ_SECTION = (By.CLASS_NAME, "Home_FAQ__3uVm4")
-    FAQ_QUESTIONS = (By.XPATH, "//div[@data-accordion-component='AccordionItemButton']")
-    FAQ_ANSWERS = (By.XPATH, "//div[@data-accordion-component='AccordionItemPanel']")
-    
-    # Кнопки заказа
-    ORDER_BUTTON_TOP = (By.XPATH, "//button[contains(@class, 'Button_Button__ra12g') and text()='Заказать']")
-    ORDER_BUTTON_BOTTOM = (By.XPATH, "(//button[contains(@class, 'Button_Button__ra12g') and text()='Заказать'])[2]")
-    
-    # Логотипы
-    SCOOTER_LOGO = (By.XPATH, "//a[@href='/']")
-    YANDEX_LOGO = (By.XPATH, "//a[@href='//yandex.ru']")
-    
-    def get_faq_question(self, index: int):
-        questions = self.find_elements(self.FAQ_QUESTIONS)
-        return questions[index]
-    
-    def get_faq_answer(self, index: int):
-        answers = self.find_elements(self.FAQ_ANSWERS)
-        return answers[index]
-    
-    def click_faq_question(self, index: int):
-        question = self.get_faq_question(index)
-        self.scroll_to_element(question)
-        question.click()
-    
-    def get_faq_answer_text(self, index: int) -> str:
-        answer = self.get_faq_answer(index)
-        return answer.text
-    
-    def click_order_button_top(self):
-        button = self.find_element(self.ORDER_BUTTON_TOP)
-        self.scroll_to_element(button)
-        button.click()
-    
-    def click_order_button_bottom(self):
-        button = self.find_element(self.ORDER_BUTTON_BOTTOM)
-        self.scroll_to_element(button)
-        button.click()
-    
-    def click_scooter_logo(self):
-        self.find_element(self.SCOOTER_LOGO).click()
-    
-    def click_yandex_logo(self):
->>>>>>> 5e94532fc497709f8b899320f9d7b50e5d8148b5
         self.find_element(self.YANDEX_LOGO).click()
